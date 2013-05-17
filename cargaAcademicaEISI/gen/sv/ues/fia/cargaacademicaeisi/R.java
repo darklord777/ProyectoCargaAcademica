d0ed41174cbab4d66dc6344c5820418bbeda2439
Alexis <alexisroke@hotmail.com>
2013-05-16 22:06:42 -0600

    carga academica

--- cargaAcademicaEISI/gen/sv/ues/fia/cargaacademicaeisi/R.java
+++ cargaAcademicaEISI/gen/sv/ues/fia/cargaacademicaeisi/R.java
@@ -33,37 +33,48 @@ public final class R {
         public static final int save=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int btnAdmonCat=0x7f090006;
+        public static final int action_settings=0x7f090010;
+        public static final int btn1TablaCiclo=0x7f090008;
+        public static final int btn2TablaCargaAcademica=0x7f090007;
+        public static final int btn3DetCargaMateria=0x7f090006;
+        public static final int btn4DetCargaActAcademica=0x7f090009;
+        public static final int btn5A_CargaAcademica=0x7f09000b;
+        public static final int btnAdmonCat=0x7f09000a;
         public static final int btnEntrarSistema=0x7f090005;
         public static final int btnNuevoDepto=0x7f090001;
-        public static final int button1_insertar_ciclo=0x7f09000b;
+        public static final int button1_insertar_ciclo=0x7f09000f;
         public static final int button2=0x7f090003;
         public static final int button3=0x7f090002;
         public static final int button4=0x7f090004;
-        public static final int button5=0x7f090007;
-        public static final int idDepto=0x7f090008;
-        public static final int nomDepto=0x7f09000a;
+        public static final int idDepto=0x7f09000c;
+        public static final int nomDepto=0x7f09000e;
         public static final int otro=0x7f090000;
-        public static final int textView1=0x7f090009;
+        public static final int textView1=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_admin_catalogos=0x7f030000;
         public static final int activity_admon_depto=0x7f030001;
         public static final int activity_carga_academica=0x7f030002;
-        public static final int activity_menu_principal=0x7f030003;
-        public static final int activity_nuevo_depto=0x7f030004;
+        public static final int activity_gestion_carga_acad=0x7f030003;
+        public static final int activity_menu_principal=0x7f030004;
+        public static final int activity_nuevo_depto=0x7f030005;
     }
     public static final class menu {
         public static final int admin_catalogos=0x7f080000;
         public static final int admon_depto=0x7f080001;
         public static final int carga_academica=0x7f080002;
-        public static final int menu_principal=0x7f080003;
-        public static final int nuevo_depto=0x7f080004;
+        public static final int gestion_carga_acad=0x7f080003;
+        public static final int menu_principal=0x7f080004;
+        public static final int nuevo_depto=0x7f080005;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int btn1TablaCiclo=0x7f060010;
+        public static final int btn2TablaCargaAcademica=0x7f060011;
+        public static final int btn3DetCargaMateria=0x7f060012;
+        public static final int btn4DetCargaActAcademica=0x7f060013;
+        public static final int btn5A_CargaAcademica=0x7f060014;
         public static final int btnAdmonCat=0x7f060005;
         /**  sistema 
          */
@@ -78,6 +89,7 @@ public final class R {
         public static final int msgidDepto=0x7f060009;
         public static final int title_activity_admin_catalogos=0x7f06000e;
         public static final int title_activity_admon_depto=0x7f060006;
+        public static final int title_activity_gestion_carga_acad=0x7f06000f;
         public static final int title_activity_menu_principal=0x7f060003;
         public static final int title_activity_nuevo_depto=0x7f060008;
         public static final int tituloDepto=0x7f06000a;
