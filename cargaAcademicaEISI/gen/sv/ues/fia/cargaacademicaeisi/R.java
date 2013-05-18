bd4f134b0168fa24efaff3eb5ffe2582a99dd75a
mario <mariosanchezsv87@gmail.com>
2013-05-18 14:55:59 -0600

    up

--- cargaAcademicaEISI/gen/sv/ues/fia/cargaacademicaeisi/R.java
+++ cargaAcademicaEISI/gen/sv/ues/fia/cargaacademicaeisi/R.java
@@ -40,43 +40,43 @@ public final class R {
         public static final int update=0x7f020009;
     }
     public static final class id {
-        public static final int BtnActAcad=0x7f0a0030;
-        public static final int BtnLocal=0x7f0a002f;
-        public static final int BtnModCurso=0x7f0a002e;
-        public static final int action_settings=0x7f0a0043;
-        public static final int btn1TablaCiclo=0x7f0a002b;
-        public static final int btn2TablaCargaAcademica=0x7f0a002a;
-        public static final int btn3DetCargaMateria=0x7f0a0029;
-        public static final int btn4DetCargaActAcademica=0x7f0a002c;
-        public static final int btn5A_CargaAcademica=0x7f0a003e;
+        public static final int BtnActAcad=0x7f0a0031;
+        public static final int BtnLocal=0x7f0a0030;
+        public static final int BtnModCurso=0x7f0a002f;
+        public static final int action_settings=0x7f0a0044;
+        public static final int btn1TablaCiclo=0x7f0a002c;
+        public static final int btn2TablaCargaAcademica=0x7f0a002b;
+        public static final int btn3DetCargaMateria=0x7f0a002a;
+        public static final int btn4DetCargaActAcademica=0x7f0a002d;
+        public static final int btn5A_CargaAcademica=0x7f0a003f;
         public static final int btnActGuardarDepto=0x7f0a0003;
         public static final int btnActualizarDepto=0x7f0a0006;
-        public static final int btnAdmonCat=0x7f0a003a;
+        public static final int btnAdmonCat=0x7f0a003b;
         public static final int btnAdmonDepto=0x7f0a0004;
         public static final int btnConsultarDepto=0x7f0a0007;
         public static final int btnEliminarDepto=0x7f0a0008;
         public static final int btnEntrarSistema=0x7f0a0009;
-        public static final int btnGLocal=0x7f0a0034;
-        public static final int btnGuardarDepto=0x7f0a0041;
-        public static final int btnLimpiaDepto=0x7f0a0042;
-        public static final int btnModAcad=0x7f0a003b;
-        public static final int btnModActAcad=0x7f0a002d;
+        public static final int btnGLocal=0x7f0a0035;
+        public static final int btnGuardarDepto=0x7f0a0042;
+        public static final int btnLimpiaDepto=0x7f0a0043;
+        public static final int btnModAcad=0x7f0a003c;
+        public static final int btnModActAcad=0x7f0a002e;
         public static final int btnNuevoDepto=0x7f0a0005;
-        public static final int btn_MenuLocal_Act=0x7f0a0038;
-        public static final int btn_MenuLocal_Cons=0x7f0a0037;
-        public static final int btn_MenuLocal_Elim=0x7f0a0036;
-        public static final int btn_MenuLocal_Ins=0x7f0a0039;
+        public static final int btn_MenuLocal_Act=0x7f0a0039;
+        public static final int btn_MenuLocal_Cons=0x7f0a0038;
+        public static final int btn_MenuLocal_Elim=0x7f0a0037;
+        public static final int btn_MenuLocal_Ins=0x7f0a003a;
         public static final int button1=0x7f0a0012;
         public static final int button1_MenuCiclo_A=0x7f0a0024;
         public static final int button1_consultarciclo=0x7f0a0016;
         public static final int button1_insertar_ciclo=0x7f0a001c;
         public static final int button2_MenuCiclo_B=0x7f0a0025;
         public static final int button2_limpiartexto_ciclo=0x7f0a0015;
-        public static final int button3=0x7f0a003d;
+        public static final int button3=0x7f0a003e;
         public static final int button3_MenuCiclo_C=0x7f0a0026;
-        public static final int button4=0x7f0a003c;
+        public static final int button4=0x7f0a003d;
         public static final int button4_MenuCiclo_D=0x7f0a0023;
-        public static final int capLocal=0x7f0a0032;
+        public static final int capLocal=0x7f0a0033;
         public static final int editText1=0x7f0a000f;
         public static final int editText1_consult_inicio=0x7f0a0019;
         public static final int editText1_fechainicio=0x7f0a0022;
@@ -85,10 +85,10 @@ public final class R {
         public static final int editText2_fechafin=0x7f0a001d;
         public static final int edtActNomDepto=0x7f0a0002;
         public static final int edtNomDepto=0x7f0a0028;
-        public static final int idDepto=0x7f0a003f;
-        public static final int idLocal=0x7f0a0031;
-        public static final int limpiarLocal=0x7f0a0033;
-        public static final int nomDepto=0x7f0a0040;
+        public static final int idDepto=0x7f0a0040;
+        public static final int idLocal=0x7f0a0032;
+        public static final int limpiarLocal=0x7f0a0034;
+        public static final int nomDepto=0x7f0a0041;
         public static final int spinner1=0x7f0a000a;
         public static final int spinner2=0x7f0a000c;
         public static final int spinner_anio_ciclo=0x7f0a001e;
@@ -96,8 +96,9 @@ public final class R {
         public static final int spinner_ciclo=0x7f0a0021;
         public static final int spinner_ciclo_consultr=0x7f0a0017;
         public static final int spnActIdDepto=0x7f0a0001;
+        public static final int spnElimDepto=0x7f0a0029;
         public static final int spnListaDeptos=0x7f0a0027;
-        public static final int textLocales=0x7f0a0035;
+        public static final int textLocales=0x7f0a0036;
         public static final int textView1=0x7f0a000b;
         public static final int textView1_consult_inicio=0x7f0a0000;
         public static final int textView2=0x7f0a000d;
@@ -148,91 +149,92 @@ public final class R {
         public static final int nuevo_depto=0x7f090010;
     }
     public static final class string {
-        public static final int TextViewLocales=0x7f070047;
+        public static final int TextViewLocales=0x7f070048;
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
         public static final int btn1TablaCiclo=0x7f070010;
-        public static final int btn1_insertar_local=0x7f070043;
+        public static final int btn1_insertar_local=0x7f070044;
         public static final int btn2TablaCargaAcademica=0x7f070011;
-        public static final int btn2_consultar_local=0x7f070044;
+        public static final int btn2_consultar_local=0x7f070045;
         public static final int btn3DetCargaMateria=0x7f070012;
-        public static final int btn3_eliminar_local=0x7f070045;
+        public static final int btn3_eliminar_local=0x7f070046;
         public static final int btn4DetCargaActAcademica=0x7f070013;
-        public static final int btn4_actualizar_local=0x7f070046;
+        public static final int btn4_actualizar_local=0x7f070047;
         public static final int btn5A_CargaAcademica=0x7f070014;
-        public static final int btnActAcad=0x7f07003e;
-        public static final int btnActualizarDepto=0x7f070030;
+        public static final int btnActAcad=0x7f07003f;
+        public static final int btnActualizarDepto=0x7f070031;
         public static final int btnAdmonCat=0x7f070005;
         /**  Alexis 
          */
-        public static final int btnAdmonDepto=0x7f07002c;
-        public static final int btnConsultarDepto=0x7f07002e;
-        public static final int btnEliminarDepto=0x7f070031;
+        public static final int btnAdmonDepto=0x7f07002d;
+        public static final int btnConsultarDepto=0x7f07002f;
+        public static final int btnEliminarDepto=0x7f070032;
         /**  sistema 
          */
         public static final int btnEntrarSistema=0x7f070004;
         public static final int btnGuardarDepto=0x7f07000c;
-        public static final int btnGuardarLocal=0x7f070039;
+        public static final int btnGuardarLocal=0x7f07003a;
         public static final int btnLimpiaDepto=0x7f07000d;
-        public static final int btnLocales=0x7f07003f;
-        public static final int btnModAcad=0x7f07003d;
-        public static final int btnModCurso=0x7f070040;
-        public static final int btnModalAcad=0x7f070041;
+        public static final int btnLocales=0x7f070040;
+        public static final int btnModAcad=0x7f07003e;
+        public static final int btnModCurso=0x7f070041;
+        public static final int btnModalAcad=0x7f070042;
         public static final int btnNuevoDepto=0x7f070007;
-        public static final int btn_limpiarLocal=0x7f07003b;
-        public static final int button1_eliminar_ciclo=0x7f070017;
-        public static final int button1_insertar_ciclo=0x7f070022;
-        public static final int button1_insertar_ciclo1=0x7f07001a;
-        public static final int button1a_insertarciclo=0x7f07001b;
+        public static final int btn_limpiarLocal=0x7f07003c;
+        public static final int button1_eliminar_ciclo=0x7f070018;
+        public static final int button1_insertar_ciclo=0x7f070023;
+        public static final int button1_insertar_ciclo1=0x7f07001b;
+        public static final int button1a_insertarciclo=0x7f07001c;
         /**  Alexis 
          */
-        public static final int button2_insertar_ciclo=0x7f070016;
-        public static final int button2_limpiartexto_ciclo=0x7f070023;
-        public static final int button2a_concultar_Ci=0x7f07002a;
-        public static final int button3_consultar_ciclo=0x7f070018;
-        public static final int button3a_eliminar_Ciclo=0x7f07002b;
-        public static final int button4_actualizar_ciclo=0x7f070019;
-        public static final int consult_fechafin_hint=0x7f070029;
-        public static final int consult_fechainicio_hint=0x7f070028;
+        public static final int button2_insertar_ciclo=0x7f070017;
+        public static final int button2_limpiartexto_ciclo=0x7f070024;
+        public static final int button2a_concultar_Ci=0x7f07002b;
+        public static final int button3_consultar_ciclo=0x7f070019;
+        public static final int button3a_eliminar_Ciclo=0x7f07002c;
+        public static final int button4_actualizar_ciclo=0x7f07001a;
+        public static final int consult_fechafin_hint=0x7f07002a;
+        public static final int consult_fechainicio_hint=0x7f070029;
         public static final int hello_world=0x7f070002;
-        public static final int msgActualizarDepto=0x7f070033;
-        public static final int msgCapLocal=0x7f070038;
+        public static final int msgActualizarDepto=0x7f070034;
+        public static final int msgCapLocal=0x7f070039;
+        public static final int msgElimDepto=0x7f070016;
         public static final int msgNomDepto=0x7f07000b;
         /**  Mario 
          */
         public static final int msgidDepto=0x7f070009;
-        public static final int msgidLocal=0x7f070037;
-        public static final int msj_fechafin_hint=0x7f070025;
-        public static final int msj_fechainicio_hint=0x7f070024;
-        public static final int nLocal=0x7f07003a;
-        public static final int spinner_anio_ciclo=0x7f07001f;
-        public static final int spinner_ciclo=0x7f070021;
-        public static final int textview_fechafin=0x7f07001d;
-        public static final int textview_fechaini=0x7f07001c;
-        public static final int textview_selecione_ciclo=0x7f070020;
-        public static final int textview_selecioneanio_ciclo=0x7f07001e;
-        public static final int title_activity_actualizar_depto=0x7f070032;
+        public static final int msgidLocal=0x7f070038;
+        public static final int msj_fechafin_hint=0x7f070026;
+        public static final int msj_fechainicio_hint=0x7f070025;
+        public static final int nLocal=0x7f07003b;
+        public static final int spinner_anio_ciclo=0x7f070020;
+        public static final int spinner_ciclo=0x7f070022;
+        public static final int textview_fechafin=0x7f07001e;
+        public static final int textview_fechaini=0x7f07001d;
+        public static final int textview_selecione_ciclo=0x7f070021;
+        public static final int textview_selecioneanio_ciclo=0x7f07001f;
+        public static final int title_activity_actualizar_depto=0x7f070033;
         public static final int title_activity_admin_catalogos=0x7f07000e;
         public static final int title_activity_admon_depto=0x7f070006;
-        public static final int title_activity_ciclo_actualizar=0x7f070035;
-        public static final int title_activity_ciclo_consultar=0x7f070026;
-        public static final int title_activity_ciclo_eliminar=0x7f070034;
+        public static final int title_activity_ciclo_actualizar=0x7f070036;
+        public static final int title_activity_ciclo_consultar=0x7f070027;
+        public static final int title_activity_ciclo_eliminar=0x7f070035;
         public static final int title_activity_ciclo_insertar=0x7f070015;
-        public static final int title_activity_ciclo_menu=0x7f070027;
-        public static final int title_activity_consultar_depto=0x7f07002d;
+        public static final int title_activity_ciclo_menu=0x7f070028;
+        public static final int title_activity_consultar_depto=0x7f07002e;
         /**  Michael 
          */
-        public static final int title_activity_eliminar_depto=0x7f070048;
+        public static final int title_activity_eliminar_depto=0x7f070049;
         public static final int title_activity_gestion_carga_acad=0x7f07000f;
-        public static final int title_activity_gestion_mod_acad=0x7f07003c;
+        public static final int title_activity_gestion_mod_acad=0x7f07003d;
         /**  Michael 
          */
-        public static final int title_activity_insertar_locales=0x7f070036;
-        public static final int title_activity_locales_menu=0x7f070042;
+        public static final int title_activity_insertar_locales=0x7f070037;
+        public static final int title_activity_locales_menu=0x7f070043;
         public static final int title_activity_menu_principal=0x7f070003;
         public static final int title_activity_nuevo_depto=0x7f070008;
         public static final int tituloDepto=0x7f07000a;
-        public static final int tvConsultarDepto=0x7f07002f;
+        public static final int tvConsultarDepto=0x7f070030;
     }
     public static final class style {
         /** 
--- cargaAcademicaEISI/src/sv/ues/fia/cargaacademicaeisi/EliminarDeptoActivity.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaacademicaeisi/EliminarDeptoActivity.java
@@ -1,15 +1,52 @@
 package sv.ues.fia.cargaacademicaeisi;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class EliminarDeptoActivity extends Activity {
+public class EliminarDeptoActivity extends Activity implements
+		OnItemSelectedListener {
+	private ControlDB helper;
+	private Spinner spnElimDepto;
+	private EditText edtNomDepto;
+	private List<String> idDeptos;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_eliminar_depto);
+		helper = new ControlDB(this);
+		spnElimDepto = (Spinner) findViewById(R.id.spnElimDepto);
+		edtNomDepto = (EditText) findViewById(R.id.edtNomDepto);
+
+		helper.abrir();
+		idDeptos = helper.getAllIdDeptos();
+		helper.cerrar();
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, idDeptos);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spnElimDepto.setAdapter(adapter);
+		spnElimDepto.setOnItemSelectedListener(this);
+	}
+
+	public void eliminarDepto(View v) {
+		Departamento departamento = new Departamento();
+		departamento.setIddepartamento(spnElimDepto.getSelectedItem()
+				.toString());
+		helper.abrir();
+		String estado = helper.eliminar(departamento);
+		helper.cerrar();
+		Toast.makeText(this, estado, Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -17,6 +54,29 @@ public class EliminarDeptoActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.eliminar_depto, menu);
 		return true;
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		String idDepto = arg0.getItemAtPosition(arg2).toString();
+		helper.abrir();
+		Departamento departamento = helper.consultarDepto(idDepto);
+		helper.cerrar();
+		if (departamento == null) {
+			Toast.makeText(
+					this,
+					"Identificador de departemento: " + idDepto
+							+ ". No existe.", Toast.LENGTH_LONG).show();
+		} else {
+			edtNomDepto.setText(departamento.getNom_depto());
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
