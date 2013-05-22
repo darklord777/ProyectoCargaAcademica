fa27ae39b46f5a744ad5fd2bbbb5c80def61f81f
Michael <miguel.a1906@gmail.com>
2013-05-22 17:43:24 -0600

    UP

--- cargaAcademicaEISI/src/sv/ues/fia/cargaacademicaeisi/ModActAcadEliminarActivity.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaacademicaeisi/ModActAcadEliminarActivity.java
@@ -1,15 +1,51 @@
 package sv.ues.fia.cargaacademicaeisi;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class ModActAcadEliminarActivity extends Activity {
+public class ModActAcadEliminarActivity extends Activity  implements OnItemSelectedListener{
 
+	private ControlDB helper;
+	private Spinner spnEliminarModAA;
+	private EditText NombreModAA_Elim;
+	private List<String> idModalAA;
+	private ArrayAdapter<String> adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mod_act_acad_eliminar);
+		
+		helper = new ControlDB(this);
+		spnEliminarModAA = (Spinner) findViewById(R.id.spn_Select_ModC);
+		NombreModAA_Elim = (EditText) findViewById(R.id.ModCursoN_Elim);
+		helper.abrir();
+		idModalAA = helper.getAllIdModCurso();
+		helper.cerrar();		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, idModalAA);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spnEliminarModAA.setAdapter(adapter);
+		spnEliminarModAA.setOnItemSelectedListener(this);
+	}
+	
+	public void eliminarModActA(View v) {
+		String estado;
+		Modalidad_Act_Acad ModalAA = new Modalidad_Act_Acad();
+		ModalAA.setIdmodalidad(spnEliminarModAA.getSelectedItem().toString());
+		helper.abrir();
+		estado = helper.eliminarModalActAcad(ModalAA);
+		helper.cerrar();
+		Toast.makeText(this, estado, Toast.LENGTH_LONG).show();
 	}
 
 	@Override
@@ -17,6 +53,24 @@ public class ModActAcadEliminarActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.mod_act_acad_eliminar, menu);
 		return true;
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		// TODO Auto-generated method stub
+		String idModActA = arg0.getItemAtPosition(arg2).toString();
+		Modalidad_Act_Acad MAA = new Modalidad_Act_Acad();
+		helper.abrir();
+		MAA = helper.consultarModActAcad(idModActA);
+		helper.cerrar();
+		NombreModAA_Elim.setText(MAA.getNom_modalidad());
+	}
+	
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
